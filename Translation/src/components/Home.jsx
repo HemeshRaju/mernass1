7421a7b72d5b294755c035d4d836281e09d1f2e2
HemeshRaju <kapallihemeshraju@gmail.com>
2025-03-15 15:47:27 +0530

    Add files via upload

---
+++ Translation/src/components/Home.jsx
@@ -0,0 +1,17 @@
+//import axios from 'axios';
+import Title from './Title';
+import TextInput from './TextInput';
+import TextOuput from './TextOuput';
+const Home = () => {
+    
+    
+  return (
+    <div>
+        <Title/>
+        <TextInput/>
+        <TextOuput/>
+    </div>
+  )
+}
+
+export default Home
